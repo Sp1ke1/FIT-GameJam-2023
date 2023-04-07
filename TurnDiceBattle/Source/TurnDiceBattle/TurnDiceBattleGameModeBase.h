af8fdd1fbb327f69736662cecc7a60f5a9f89e3c
cherkand <cherkand@fit.cvut.cz>
2023-04-07 14:56:01 +0200

    Added blank project and gitignore

---
+++ TurnDiceBattle/Source/TurnDiceBattle/TurnDiceBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurnDiceBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURNDICEBATTLE_API ATurnDiceBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
