c0e932785942793167c69d26382dd465d1967603
Novikov Roman <roman.novikov@victoriavr.com>
2023-04-10 15:34:00 +0200

    Tricky plugins removed

--- TurnDiceBattle/Plugins/TrickyInteractionSystem/Source/TrickyInteractionSystem/Private/InteractionInterface.cpp
+++
@@ -1,6 +0,0 @@
-// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
-
-
-#include "InteractionInterface.h"
-
-// Add default functionality here for any IInteractionInterface functions that are not pure virtual.
--- TurnDiceBattle/Plugins/TrickyUserInterface/Source/TrickyUserInterface/Private/TransitionScreenWidget.cpp
+++
@@ -1,5 +0,0 @@
-// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
-
-
-#include "TransitionScreenWidget.h"
-
